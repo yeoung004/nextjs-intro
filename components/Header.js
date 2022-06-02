ec8c7cb74b037b1d195277d26e51a344825c9577
Youngsung Choi <dudtjd4149@gmail.com>
2022-06-02 15:54:15 +0900

    fix: fix untracked file
    Fix untracked file because gitignore isn't working

--- components/Header.js
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head'
-
-export default function Header({title}) {
-  return(
-    <Head>
-      <title>{title} | Next Moives</title>
-    </Head>
-  )
-};
--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import Layout from "../components/Layout";
-
-export default function Myapp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
-};
